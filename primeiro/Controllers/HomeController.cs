455a3536080a4c425b77ab122130cbdc14ac1b71
henrikort <henrikort13@hotmai.com>
2024-06-20 21:12:25 -0300

    aula3

--- primeiro/Controllers/HomeController.cs
+++ primeiro/Controllers/HomeController.cs
@@ -14,12 +14,49 @@ namespace primeiro.Controllers
         }
         [HttpGet]
         [Route("CalculateMedia")]
-        public float CalculateMedia(float n1, float n2, float n3)
+        public string CalculateMedia(float n1, float n2, float frequencia)
         {
-            float resultado;
-            resultado = (n1 + n2 + n3) / 3;
-            return resultado;        
+           
+            float media = (n1 + n2 ) / 2;
+
+            if (media >= 7 && frequencia >= 75)
+            {
+                return "aprovado";
+            }
+            else 
+            {
+                return "reprovado";
+            }
         }
+
+        [HttpGet]
+        [Route("CalculateMedia3")]
+        public string CalculateMedia3(float n1, float n2)
+        {
+
+            float media = (n1 + n2) / 2;
+
+            if (media >= 7 )
+            {
+                return "aprovado";
+            }
+            if (media <= 6 && media >= 5)
+            {
+                return "revisao";
+
+            }
+            else 
+            {
+                return "reprovado";
+            }
+          
+        }
+
+
+
+
+
+
 
         [HttpGet]
         [Route("GetValue")]
